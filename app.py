004da003305154fb7d5a9e2c6d1f1068ec04b254
Zubair Tarif <53663854+zubairtarif@users.noreply.github.com>
2022-06-12 17:09:41 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "abc"
+
+@app.route("/hello")
+def index():
+	flash("What's your name?")
+	return render_template("index.html") #because we saved an index file udner templates folder, flask will be able to find it
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+	return render_template("index.html")
